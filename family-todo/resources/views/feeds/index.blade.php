cb3f88eea4d677c83006f9e9d0e628e2ba04a9e3
Jimies22 <2201101389@student.buksu.edu.ph>
2025-06-14 06:21:00 +0800

    Feed UI added

--- family-todo/resources/views/feeds/index.blade.php
+++ family-todo/resources/views/feeds/index.blade.php
@@ -1,3 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Feed') }}
+        </h2>
+    </x-slot>
 @foreach ($posts as $post)
     <div class="bg-white p-4 rounded-xl shadow mb-4">
         <div class="font-bold">{{ $post->user->name }}'s Weekly Tasks</div>
@@ -12,3 +18,5 @@
         <small class="text-gray-500">Posted {{ $post->created_at->diffForHumans() }}</small>
     </div>
 @endforeach
+
+</x-app-layout>
