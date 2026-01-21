38b37160940d6751c9bf1816666c7a63ee44a20e
2201101389 <2201101389@student.buksu.edu.ph>
2026-01-21 03:39:07 +0000

    fireEnv stablize based

--- family-todo/flake.nix
+++ family-todo/flake.nix
@@ -1,32 +1,38 @@
-
 {
   description = "A PHP project for Family-Todo-App";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-        php = pkgs.php82;
-        composer = pkgs.composer;
-      in
-      {
-        devShell = pkgs.mkShell {
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        php = pkgs.php82.withExtensions ({ enabled, all }: enabled ++ [
+          all.curl
+          all.mbstring
+          all.openssl
+          all.pdo
+          all.pdo_mysql
+          all.tokenizer
+          all.xml
+          all.zip
+        ]);
+      in {
+        devShells.default = pkgs.mkShell {
           buildInputs = [
             php
-            composer
+            pkgs.php82Packages.composer
             pkgs.nodejs-18_x
-            pkgs.npm
+            pkgs.nodejs-18_x
           ];
-          
+
           shellHook = ''
             export PATH=$PWD/vendor/bin:$PATH
+            echo "🚀 PHP $(php -r 'echo PHP_VERSION;') | Node $(node -v)"
           '';
         };
       }
